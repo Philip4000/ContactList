664d6e0be04fbb961903d73906a6f68a4e9fe8e9
Philip4000 <6427676@bk.ru>
2022-12-18 23:18:44 +0300

    initial commit

---
+++ ContactList/Models/Person.swift
@@ -0,0 +1,46 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Philip Noskov on 18.12.2022.
+//
+
+import Foundation
+
+
+struct Person {
+    let name: String
+    let surname: String
+    let email: String
+    let phone: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+    
+    static func getPersonList() -> [Person] {
+        let data = DataStore()
+        var persons: [Person] = []
+        
+        let shuffledNames = data.names.shuffled()
+        let shuffledSurnames = data.names.shuffled()
+        let shuffledEmails = data.emails.shuffled()
+        let shuffledPhones = data.phones.shuffled()
+        
+        let minCount = min(shuffledNames.count, shuffledSurnames.count, shuffledEmails.count, shuffledPhones.count)
+        
+        
+        for i in 0...minCount {
+            let person = Person(
+                name: shuffledNames[i],
+                surname: shuffledSurnames[i],
+                email: shuffledEmails[i],
+                phone: shuffledPhones[i])
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+    
+}
---
+++ ContactList/Services/DataStore.swift
@@ -0,0 +1,64 @@
+//
+//  DataStore.swift
+//  ContactList
+//
+//  Created by Philip Noskov on 18.12.2022.
+//
+
+import Foundation
+
+class DataStore {
+    let names = [
+        "John",
+        "Ted",
+        "Sam",
+        "Bruce",
+        "Phil",
+        "Tim",
+        "Carl",
+        "Alan",
+        "Tom",
+        "Tom"
+    ]
+    
+    let surnames = [
+        "Appleseed",
+        "Black",
+        "Cook",
+        "Whiliams",
+        "Butler",
+        "Aniston",
+        "Carry",
+        "Jobs",
+        "Jackson",
+        "Smith"
+    ]
+    
+    let emails = [
+        "aaa@gmail.com",
+        "bbb@gmail.com",
+        "ccc@gmail.com",
+        "ddd@gmail.com",
+        "eee@gmail.com",
+        "fff@gmail.com",
+        "gggg@gmail.com",
+        "hhh@gmail.com",
+        "iii@gmail.com",
+        "jjj@gmail.com"
+    ]
+    
+    let phones = [
+        "+71111111",
+        "+72222222",
+        "+73333333",
+        "+74444444",
+        "+75555555",
+        "+76666666",
+        "+77777777",
+        "+78888888",
+        "+79999999",
+        "+71234567"
+    ]
+    
+    
+}
